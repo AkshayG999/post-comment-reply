bbb8ce4fee59904393ca945db1ed8fa0012296de
Akshay <108966177+AkshayG999@users.noreply.github.com>
2023-02-27 21:27:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const port=process.env.PORT || 4000;
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://client.dggtyh77ra2h8.amplifyapp.com'
 }))
 
 
