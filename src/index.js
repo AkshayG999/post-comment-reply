a3029fa7e4d42f481b8e1c6f71b6823ed8b07568
Akshay <108966177+AkshayG999@users.noreply.github.com>
2023-02-27 19:33:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config()
+const port=process.env.PORT || 4000;
 
 app.use(cors({
     origin: 'http://localhost:3000'
@@ -13,7 +14,7 @@ app.use(cors({
 app.use(express.json())
 
 mongoose.set('strictQuery', false)
-mongoose.connect(process.env.url, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .then(() => console.log("MongoDB is Connected"))
     .catch((err) => console.log(err))
 
@@ -21,4 +22,4 @@ app.use('/', require('./routes/route'))
 
 app.use('/*', (req, res) => res.status(400).send({ message: "Invalid HTTP End Point" }))
 
-app.listen(process.env.port, () => console.log("Server Is Running On " + process.env.port))
+app.listen(port, () => console.log("Server Is Running On " +port))
