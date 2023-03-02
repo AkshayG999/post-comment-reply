cc071a20726e9df624569d0aa5e2af48bbfa5421
Akshay <108966177+AkshayG999@users.noreply.github.com>
2023-03-02 13:18:52 +0000

    Navbar push

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,8 +11,8 @@ import {useNavigate } from "react-router-dom";
 export default function MenuAppBar() {
     const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState(null);
-    const navigate = useNavigate();
- 
+    const navigate = useNavigate(); 
+
     const handleAddPost = (e) => {
         // handleCloseUserMenu()
         navigate('/add-post')
